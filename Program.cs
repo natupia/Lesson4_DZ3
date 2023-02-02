ebcfa2a4d266309e261200e4de9a55d3f2d11cb1
Natalia <nataliahola@yandex.ru>
2023-02-02 22:34:57 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] CreatArray(int size)
+{
+    int[] massiv = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        massiv[i] = rnd.Next(1, 9);
+    }
+    return massiv;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " " );
+    }
+}
+
+int[] array = CreatArray(8);
+PrintArray(array);
